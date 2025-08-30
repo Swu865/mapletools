bf49fb5185ceb631c01f79b7943ed7ef2414d6fb
Swu865 <swu865@aucklanduni.ac.nz>
2025-08-30 18:58:09 +1200

    edit boss monster damage > boss damage

--- autocubing/autocubing/main.py
+++ autocubing/autocubing/main.py
@@ -41,7 +41,7 @@ VALID_KEYS = ["Skill Cooldown: -2","Skill Cooldown: -1",'Magic ATT: +12%', 'Magi
 
 
 def match_valid_key(input_str: str, threshold=80) -> str | None:
-
+    # ignore the strings without %      
     if not re.search(r'[\+\-]\d+%$', input_str) and "Skill Cooldown" not in input_str:
         return "pass"
 
